fa0dbccc45a707461f72e364faed5c16b734e4fe
Mibo <meebsawosika@gmail.com>
2025-10-30 22:05:30 -0600

    feat: implement location searching

--- src/api.js
+++ src/api.js
@@ -1,13 +1,17 @@
 async function getWeatherData(location) {
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}next7days?unitGroup=metric&include=days%2Ccurrent&key=C8A8ZX454796DUF52CDNCX2KK&contentType=json`) 
-    const data = await response.json();
-    return data;
+  const response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}next7days?unitGroup=metric&include=days%2Ccurrent&key=C8A8ZX454796DUF52CDNCX2KK&contentType=json`,
+  );
+  const data = await response.json();
+  return data;
 }
 
 export async function currentConditions(location) {
-    const data = await getWeatherData(location);
-    const conditions = data.currentConditions;
-    return conditions
+  const data = await getWeatherData(location);
+  const conditions = data.currentConditions;
+  const days = data.days
+    
+  return {conditions, days}
 }
 
-//Properties to pull out datetime, temp, feelslike, conditions, icon
+//Properties to pull out datetime, temp, feelslike, conditions, icon. Properites to pull out of days array tempmax, temp min, icon
--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import { currentConditions } from "./api";
+import "./styles.css"
 
-currentConditions("calgary")
+// const { conditions, days } = await currentConditions("calgary");
+const search = document.querySelector("button");
+const input = document.querySelector("input");
+async function handleSearch() {
+    try {
+        const { conditions, days } = await currentConditions(input.value);
+        refreshDom(conditions, days);
+    } catch (err) {
+        console.error("Failed to fetch weather data", err)
+    }
+}
+function refreshDom(conditions,days) {
+    console.log(conditions,days)
+}
+search.addEventListener("click", handleSearch);
