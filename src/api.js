658f023e80ee909cbfd3e7693dcdba151b99a4a5
Mibo <meebsawosika@gmail.com>
2025-11-01 01:55:09 -0600

    feat: add in error handling

--- src/api.js
+++ src/api.js
@@ -1,17 +1,21 @@
 async function getWeatherData(location) {
-  const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}next7days?unitGroup=metric&include=days%2Ccurrent&key=C8A8ZX454796DUF52CDNCX2KK&contentType=json`,
-  );
-  const data = await response.json();
-  return data;
+  try {
+      const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}next7days?unitGroup=metric&include=days%2Ccurrent&key=C8A8ZX454796DUF52CDNCX2KK&contentType=json`,
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Failed to fetch with error: ", error)
+  }
 }
 
 export async function currentConditions(location) {
   const data = await getWeatherData(location);
   const conditions = data.currentConditions;
-  const days = data.days
-    
-  return {conditions, days}
+  const days = data.days;
+
+  return { conditions, days };
 }
 
 //Properties to pull out datetime, temp, feelslike, conditions, icon. Properites to pull out of days array tempmax, temp min, icon
